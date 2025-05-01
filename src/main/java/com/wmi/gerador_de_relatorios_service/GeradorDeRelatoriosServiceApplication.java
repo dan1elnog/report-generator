0bad11ae2ce619c30f13ffa2606e5dc3f12dadae
Daniel <daniel.00msn@gmail.com>
2025-05-01 00:01:56 -0300

    primeiro commit

---
+++ src/main/java/com/wmi/gerador_de_relatorios_service/GeradorDeRelatoriosServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wmi.gerador_de_relatorios_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeradorDeRelatoriosServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeradorDeRelatoriosServiceApplication.class, args);
+	}
+
+}
