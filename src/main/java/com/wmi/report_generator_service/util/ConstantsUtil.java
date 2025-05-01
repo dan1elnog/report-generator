31cfb114872c0c50e626072a110e2d3379edfc3e
Daniel <daniel.00msn@gmail.com>
2025-05-01 14:50:44 -0300

    finalizando asjustes para a exibição do excel

--- src/main/java/com/wmi/report_generator_service/util/ConstantsUtil.java
+++ src/main/java/com/wmi/report_generator_service/util/ConstantsUtil.java
@@ -8,6 +8,7 @@ public class ConstantsUtil {
 
     public static final Integer INITIAL_POSITION_EXCEL_HEADER = 6;
     public static final Integer INITIAL_POSITION_EXCEL_BODY = 7;
+    public static final Integer TOTAL_COLUMNS_EXCEL_REPORT = 10;
 
     public static final List<String> EXCEL_REPORT_HEADERS = List.of("Paciente", "Empresa", "Exame", "Indicação",
             "Preço", "Data exame", "Data laudo", "Status", "Data criação exame", "Usuário");
@@ -15,7 +16,7 @@ public class ConstantsUtil {
     public static final List<ExcelReportDTO> EXCEL_REPORT_MOCK = List.of(
             new ExcelReportDTO("ADEILSON ADELE", "BRASIL MADEIRAS PARANA EIRELI BRASIL MADEIRAS PARANA EIRELI BRASIL MADEIRAS PARANA EIRELI", "Eletrocardiograma", "Indicação", "4,5", "03-04-2025", "10-04-2025 15:54", "LAUDADO", "10-04-2025 10:40", "ESPAÇO SST ARAPONGAS"),
             new ExcelReportDTO("ADEILSON", "BRASIL MADEIRAS PARANA EIRELI", "Eletrocardiograma", "Indicação", "4,5", "03-04-2025", "10-04-2025 15:54", "LAUDADO", "10-04-2025 10:40", "ESPAÇO SST ARAPONGAS"),
-            new ExcelReportDTO("ADEILSON", "BRASIL MADEIRAS PARANA EIRELI", "Eletrocardiograma", "Indicação", "4,5", "03-04-2025", "10-04-2025 15:54", "LAUDADO", "10-04-2025 10:40", "ESPAÇO SST ARAPONGAS"),
+            new ExcelReportDTO("ADEILSON", "BRASIL MADEIRAS PARANA EIRELI", null, "Indicação", "4,5", "03-04-2025", "10-04-2025 15:54", "LAUDADO", "10-04-2025 10:40", "ESPAÇO SST ARAPONGAS"),
             new ExcelReportDTO("ADEILSON", "BRASIL MADEIRAS PARANA EIRELI", "Eletrocardiograma", "Indicação", "4,5", "03-04-2025", "10-04-2025 15:54", "LAUDADO", "10-04-2025 10:40", "ESPAÇO SST ARAPONGAS"),
             new ExcelReportDTO("ADEILSON", "BRASIL MADEIRAS PARANA EIRELI", "Eletrocardiograma", "Indicação", "4,5", "03-04-2025", "10-04-2025 15:54", "LAUDADO", "10-04-2025 10:40", "ESPAÇO SST ARAPONGAS")
     );
--- src/main/java/com/wmi/report_generator_service/util/ExcelUtil.java
+++ src/main/java/com/wmi/report_generator_service/util/ExcelUtil.java
@@ -13,17 +13,17 @@ import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-import static com.wmi.report_generator_service.util.ConstantsUtil.INITIAL_POSITION_EXCEL_BODY;
-import static com.wmi.report_generator_service.util.ConstantsUtil.INITIAL_POSITION_EXCEL_HEADER;
+import static com.wmi.report_generator_service.util.ConstantsUtil.*;
 import static org.springframework.http.MediaType.APPLICATION_OCTET_STREAM;
 
 @Service
 public class ExcelUtil {
 
     public ExcelReportResponseDTO generateExcel(List<String> columnNames, List<ExcelReportDTO> values) {
-
         try(Workbook workbook = new HSSFWorkbook()) {
             Sheet sheet = workbook.createSheet("planilha");
             generateHeader(workbook, sheet, columnNames);
@@ -77,29 +77,65 @@ public class ExcelUtil {
         Row header = sheet.createRow(INITIAL_POSITION_EXCEL_HEADER);
         int index = 0;
 
+        List<Integer> cellSizes= new ArrayList<>();
+
         for (String name : names) {
             Cell cell = header.createCell(index);
             cell.setCellValue(name);
             cell.setCellStyle(cellStyle);
+            cellSizes.add(name.length());
             index++;
+        }
+
+        int qtdColumns = sheet.getRow(INITIAL_POSITION_EXCEL_HEADER).getPhysicalNumberOfCells();
+        for(int i = 0; i < qtdColumns; i++ ) {
+            int currentMax = cellSizes.get(i);
+            for (int rowIndex = 1; rowIndex <= sheet.getLastRowNum(); rowIndex++) {
+                Row row = sheet.getRow(rowIndex);
+                if (row != null) {
+                    Cell cell = row.getCell(i);
+                    if (cell != null && cell.getCellType() == CellType.STRING) {
+                        int length = cell.getStringCellValue().length();
+                        currentMax = Math.max(currentMax, length);
+                    }
+                }
+            }
+            sheet.setColumnWidth(i, (currentMax + 2) * 256);
         }
     }
 
     private void generateCellValues(Sheet sheet, List<ExcelReportDTO> values){
         int rowIdx = INITIAL_POSITION_EXCEL_BODY;
+        int totalColumns = TOTAL_COLUMNS_EXCEL_REPORT;
+        List<Integer> cellSizes = new ArrayList<>(Collections.nCopies(totalColumns, 0));
 
         for (ExcelReportDTO value : values) {
             Row row = sheet.createRow(rowIdx++);
-            row.createCell(0).setCellValue(value.getPatient());
-            row.createCell(1).setCellValue(value.getCompany());
-            row.createCell(2).setCellValue(value.getExam());
-            row.createCell(3).setCellValue(value.getIndication());
-            row.createCell(4).setCellValue(value.getPrice());
-            row.createCell(5).setCellValue(value.getExamDate());
-            row.createCell(6).setCellValue(value.getMedicalReportDate());
-            row.createCell(7).setCellValue(value.getStatus());
-            row.createCell(8).setCellValue(value.getExamCreationDate());
-            row.createCell(9).setCellValue(value.getUser());
+            String[] fields = {
+                    value.getPatient(),
+                    value.getCompany(),
+                    value.getExam(),
+                    value.getIndication(),
+                    value.getPrice(),
+                    value.getExamDate(),
+                    value.getMedicalReportDate(),
+                    value.getStatus(),
+                    value.getExamCreationDate(),
+                    value.getUser()
+            };
+
+            for (int col = 0; col < totalColumns; col++) {
+                String text = fields[col] != null ? fields[col] : "-";
+                row.createCell(col).setCellValue(text);
+
+                int actualSize = cellSizes.get(col);
+                cellSizes.set(col, Math.max(actualSize, text.length()));
+            }
+        }
+
+        // Ajustar largura das colunas com margem extra
+        for (int i = 0; i < totalColumns; i++) {
+            sheet.setColumnWidth(i, (cellSizes.get(i) + 10) * 256);
         }
     }
 }
